48c308b837cec88794ba3b8a73121f93eb30aa59
Fernando Tesone <fenatesone@gmail.com>
2025-05-18 19:20:36 -0300

    Show new work experience information

--- src/features/work-experience/WorkExperience.js
+++ src/features/work-experience/WorkExperience.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import Section from '../section/Section';
-import List from '@material-ui/core/List';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
 import experience from './data';
 import PropTypes from 'prop-types';
+import { Accordion, AccordionDetails, AccordionSummary, Chip, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 
 const months = [null, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 const WorkExperienceItem = ({}, {t}) =>
-    <List>
+    <>
         {experience.map(job =>
-            <ListItem key={job.id}>
-                <ListItemIcon>{job.icon}</ListItemIcon>
-                <ListItemText primary={t(job.rol)} secondary={job.workplace} />
-                <ListItemText secondary={`${t(months[job.start.month])} ${job.start.year} – ${job.end ? t(months[job.end.month])+' '+job.end.year : t('Present')}`} style={{marginLeft: '10px', textAlign: 'right'}} />
-            </ListItem>
+            <Accordion key={job.id} variant="outlined">
+                <AccordionSummary expandIcon={<ExpandMoreIcon className="not-print" />}>
+                    <ListItemIcon>{job.icon}</ListItemIcon>
+                    <ListItemText primary={t(job.rol)} secondary={job.workplace} />
+                    <ListItemText secondary={`${t(months[job.start.month])} ${job.start.year} – ${job.end ? t(months[job.end.month])+' '+job.end.year : t('Present')}`} style={{marginLeft: '10px', textAlign: 'right'}} />
+                </AccordionSummary>
+                <AccordionDetails className="work-experience-details">
+                    <div>
+                        {job.tasks && (
+                            <ul className="tasks">
+                                {job.tasks.map((task, idx) => <li key={idx}>{t(task)}</li>)}
+                            </ul>
+                        )}
+                        {job.technologies && (
+                            <div className="techs">
+                                {job.technologies.map(tech => (
+                                    <Chip key={tech} label={tech} variant="outlined" size="small" className="leave-border" />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </AccordionDetails>
+            </Accordion>
         )}
-    </List>
+    </>
 
 WorkExperienceItem.contextTypes = {
     t: PropTypes.func,
@@ -29,7 +46,12 @@ WorkExperienceItem.contextTypes = {
 
 
 export const WorkExperience = (props, {t}) =>
-    <Section title={t('Work Experience')} body={<WorkExperienceItem />} {...props} />
+    <div style={{ marginBottom: '20px' }}>
+        <Typography variant="h2" style={{fontSize: '3rem'}}>
+            {t('Work Experience')}
+        </Typography>
+        <WorkExperienceItem />
+    </div>
 
 WorkExperience.contextTypes = {
     t: PropTypes.func,
